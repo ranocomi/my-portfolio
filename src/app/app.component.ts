170295c12a2ae8c44cbc12d8b7a5985af43a2062
Sidar Dalli <sidardalli@gmail.com>
2024-02-17 15:44:43 +0100

    setup routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {MainPageComponent} from "./main-page/main-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainPageComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {MainPageComponent} from "./main-page/main-page.component";
+import {ExperienceComponent} from "./experience/experience.component";
+import {SkillsComponent} from "./skills/skills.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: 'experience', component: ExperienceComponent},
+  {path: 'skills', component: SkillsComponent}
+];
