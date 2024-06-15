8790c5b246246f67a56edfe6110a715f43bb3749
Sidar Dalli <sidardalli@gmail.com>
2024-06-15 20:24:11 +0200

    Make navbar reactive

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import {ComponentType} from "../interface/componentType";
+import {NgForOf} from "@angular/common";
+import {Router} from "@angular/router";
 
+// @ts-ignore
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
+  tabs: ComponentType[] = [
+    {
+      name: 'Introduction',
+      path: '/'
+    },
+    {
+      name: 'Experience',
+      path: '/experience'
+    },
+    {
+      name: 'Skills',
+      path: '/skills'
+    }
+  ];
 
+  constructor(private router: Router) {}
+
+  isActive(path: string){
+    return this.router.url === path;
+  }
 }
