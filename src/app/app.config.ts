72dc36b90294c31f75335441807172132e395203
Sidar Dalli <sidardalli@gmail.com>
2024-07-01 18:35:00 +0200

    Optimize page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {CacheInterceptor} from "./http-interceptors/cache.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CacheInterceptor,
+      multi: true
+    }
+  ]
 };
